72077a9a0000b49b02ef84419856e27a1c953854
HikaruKusanagi <hikarugp24@gmail.com>
2022-06-22 22:38:13 +0900

    no message

--- lib/home.dart
+++ lib/home.dart
@@ -45,9 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    myHand = '✋';
-                    setState((){});
-                    print('✋');
+                    selectHand('✋');
                   },
                   child: Text('✋'),
                 ),
