a197c2ab012e511ee6f0fafa2490c0f983172340
HikaruKusanagi <hikarugp24@gmail.com>
2022-06-24 08:39:43 +0900

    no message

---
+++ lib/prefectures47.dart
@@ -0,0 +1,163 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class  Prefectures47 extends StatefulWidget {
+  const Prefectures47 ({Key? key}) : super(key: key);
+
+  @override
+  State<Prefectures47> createState() => _Prefectures47State();
+}
+
+class _Prefectures47State extends State<Prefectures47> {
+
+  String computerHand = '✊';
+  String myHand = '✊';
+  String result = '引き分け';
+
+  void selectHand(String selectedHand) {
+    myHand = selectedHand;
+    generateComputerHand();
+    judge();
+    setState((){});
+  }
+
+  void generateComputerHand() {
+    final randomNumber = Random().nextInt(3);
+    computerHand = randomNumberToHand(randomNumber);
+  }
+
+
+  String randomNumberToHand(int randomNumber) {
+    switch (randomNumber) {
+      case 0:
+        return '✊';
+      case 1:
+        return '✌️';
+      case 2:
+        return '✋';
+      default:
+        return '✊';
+    }
+  }
+
+  void judge() {
+    // if (computerHand == myHand) {
+    //   result = '引き分け';
+    // } else if (myHand == '✊' && computerHand == '✌️') {
+    //   result = '勝ち';
+    // } else if (myHand == '✌️' && computerHand == '✋️') {
+    //   result = '勝ち';
+    // } else if (myHand == '✋️' && computerHand == '✊') {
+    //   result = '勝ち';
+    // }
+    if (computerHand == myHand) {
+      result = '引き分け';
+    } else if (
+    myHand == '✊' && computerHand == '✌️' ||
+        myHand == '✌️' && computerHand == '✋️' ||
+        myHand == '✋️' && computerHand == '✊'
+    ) {
+      result = '勝ち';
+    } else {
+      result = '負け';
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('じゃんけん'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 20),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 60),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 60),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 60),
+              ],
+            ),
+            Text(result,style: TextStyle(fontSize: 32),),
+            SizedBox(height: 32),
+            Text(computerHand,style: TextStyle(fontSize: 32),),
+            SizedBox(height: 40),
+            Text(myHand,style: TextStyle(fontSize: 32),),
+            SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('✊');
+                  },
+                  child: Text('✊'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('✌️');
+                  },
+                  child: Text('✌️'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('✋');
+                  },
+                  child: Text('✋'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
