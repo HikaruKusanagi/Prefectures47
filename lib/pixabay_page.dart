d2387a0e6b41d6a015bf3a0c7f57b91d23ecd2f3
HikaruKusanagi <hikarugp24@gmail.com>
2022-06-25 19:34:13 +0900

    test

--- lib/pixabay_page.dart
+++ lib/pixabay_page.dart
@@ -12,11 +12,13 @@ class _PixabayPageState extends State<PixabayPage> {
   // 初めは空のListを入れておく
   List hits = [];
 
-  Future <void> fetchImage() async {
-    Response response = await Dio().get('https://pixabay.com/api/?key=28219540-f0f906ca4c183c521352d1aa0&q=yellow+flowers&image_type=photo&pretty=true');
+  Future<void> fetchImage() async {
+    Response response = await Dio().get(
+        'https://pixabay.com/api/?key=28219540-f0f906ca4c183c521352d1aa0&q=yellow+flowers&image_type=photo&pretty=true');
     hits = response.data['hits'];
-    setState((){});
+    setState(() {});
   }
+
   @override
   void initState() {
     super.initState();
@@ -28,12 +30,13 @@ class _PixabayPageState extends State<PixabayPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-          itemCount: hits.length,
-          itemBuilder: (context, index) {
-            Map<String,dynamic> hit = hits[index];
-            return Image.network(hit['previewURL']);
-          },
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemCount: hits.length,
+        itemBuilder: (context, index) {
+          Map<String, dynamic> hit = hits[index];
+          return Image.network(hit['previewURL']);
+        },
       ),
     );
   }
