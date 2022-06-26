0b0431dfd03eced53aac21f96ef0a78b4246507f
HikaruKusanagi <hikarugp24@gmail.com>
2022-06-26 10:45:28 +0900

    no message

--- lib/prefectures47.dart
+++ lib/prefectures47.dart
@@ -67,12 +67,12 @@ class _Prefectures47State extends State<Prefectures47> {
         title: Text('Prefecter47'),
       ),
       body: Column(children: [
-        SizedBox(height: 20),
+        SizedBox(height: 120),
         Center(
           child: Text('TodofuQuiz',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                fontSize: 32,
+                fontSize: 42,
               )),
         ),
       ]),
