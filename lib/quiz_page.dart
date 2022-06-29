917769c817f48f8750eb30b53446b339231d8fe9
HikaruKusanagi <hikarugp24@gmail.com>
2022-06-29 17:55:46 +0900

    no message

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -13,6 +13,7 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   String? _imgURL;
+  String? _imgURL1;
   List<String> imgURLs = [
     //ここに表示したい画像URLを追加
     'images/1_hokkaidou.png',
@@ -21,7 +22,6 @@ class _QuizPageState extends State<QuizPage> {
   ];
 
   final ram = Random(DateTime.now().millisecondsSinceEpoch);
-  
 
   @override
   Widget build(BuildContext context) {
