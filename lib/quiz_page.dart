4a56e83252b1dcc38d9fade6b831d8b2b6ecd87e
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-01 17:25:43 +0900

    no message

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -20,7 +20,7 @@ class _QuizPageState extends State<QuizPage> {
       'images/1_hokkaidou.png',
       'images/2_touhoku1__aomori.png',
       'images/2_touhoku2__iwate.png',
-
+      'images/2_touhoku3__miyagi.png',
       'images/2_touhoku4__akita.png',
     ];
 
