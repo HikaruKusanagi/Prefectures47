1fa4a9177cb88cc4180e332193dceb9b0094d7dc
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-01 17:19:40 +0900

    no message

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -22,7 +22,6 @@ class _QuizPageState extends State<QuizPage> {
       'images/2_touhoku2__iwate.png',
       'images/2_touhoku3__miyagi.png',
       'images/2_touhoku4__akita.png',
-      'images/2_touhoku5__yamagata.png',
     ];
 
     final ram = Random(
