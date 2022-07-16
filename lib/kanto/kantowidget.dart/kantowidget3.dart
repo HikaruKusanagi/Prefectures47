5ad9493e9380ac24b63237a859e1764cb53478c2
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-17 00:26:33 +0900

    no message

---
+++ lib/kanto/kantowidget.dart/kantowidget3.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:prefectures47/name_of_prefectures.dart';
+
+class KantoWidget3 extends StatelessWidget {
+  const KantoWidget3({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 20),
+        Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              side: BorderSide(
+                color: Colors.black, //色
+                width: 2, //太さ
+              ),
+            ),
+            child: Text(
+              NameOfPrefectures.kanagawa,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => print('クリックされました'),
+          ),
+        ),
+        SizedBox(width: 30),
+      ],
+    );
+  }
+}
