c8fefe36ba73e20fbdd13767cb24c9d5d4f961bb
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-19 23:22:31 +0900

    no message

---
+++ lib/kinki/kinki.widget/kinkiwidget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:prefectures47/name_of_prefectures.dart';
+
+class KinkiWidget extends StatelessWidget {
+  const KinkiWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 20),
+        Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              side: BorderSide(
+                color: Colors.black, //色
+                width: 2, //太さ
+              ),
+            ),
+            child: Text(
+              NameOfPrefectures.mie,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => print('クリックされました'),
+          ),
+        ),
+        SizedBox(width: 30),
+        Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              side: BorderSide(
+                color: Colors.black, //色
+                width: 2, //太さ
+              ),
+            ),
+            child: Text(
+              NameOfPrefectures.shiga,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => print('クリックされました'),
+          ),
+        ),
+      ],
+    );
+  }
+}
