36c46cc1870891a7ee3e8e6941fa6c3938850949
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-19 23:22:04 +0900

    no message

---
+++ lib/chugoku/chugoku.widget/chugokuwidget5.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:prefectures47/name_of_prefectures.dart';
+
+class ChugokuWidget5 extends StatelessWidget {
+  const ChugokuWidget5({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 20),
+        Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              side: BorderSide(
+                color: Colors.black, //色
+                width: 2, //太さ
+              ),
+            ),
+            child: Text(
+              NameOfPrefectures.gifu,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => print('クリックされました'),
+          ),
+        ),
+        SizedBox(width: 30),
+        Row(
+          children: [
+            Center(),
+          ],
+        ),
+      ],
+    );
+  }
+}
