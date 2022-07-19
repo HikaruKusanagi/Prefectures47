1fab242bc68cb36d0d89295fcf3e9279999e141d
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-19 23:21:57 +0900

    no message

---
+++ lib/chugoku/chugoku.widget/chugokuwidget4.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:prefectures47/name_of_prefectures.dart';
+
+class ChugokuWidget4 extends StatelessWidget {
+  const ChugokuWidget4({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 20),
+        Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              side: BorderSide(
+                color: Colors.black, //色
+                width: 2, //太さ
+              ),
+            ),
+            child: Text(
+              NameOfPrefectures.shizuoka,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => print('クリックされました'),
+          ),
+        ),
+        SizedBox(width: 30),
+        Row(
+          children: [
+            Center(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  side: BorderSide(
+                    color: Colors.black, //色
+                    width: 2, //太さ
+                  ),
+                ),
+                child: Text(
+                  NameOfPrefectures.aichi,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () => print('クリックされました'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
