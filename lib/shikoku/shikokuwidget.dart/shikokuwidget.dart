3b17d238b51e6d03b8e4f4d9da859b43de0aa8ec
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-20 21:52:00 +0900

    no message

---
+++ lib/shikoku/shikokuwidget.dart/shikokuwidget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:prefectures47/name_of_prefectures.dart';
+
+class ShikokuWidget extends StatelessWidget {
+  const ShikokuWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 20),
+        Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              side: BorderSide(
+                color: Colors.black, //色
+                width: 2, //太さ
+              ),
+            ),
+            child: Text(
+              NameOfPrefectures.kagawa,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => print('クリックされました'),
+          ),
+        ),
+        SizedBox(width: 30),
+        Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              side: BorderSide(
+                color: Colors.black, //色
+                width: 2, //太さ
+              ),
+            ),
+            child: Text(
+              NameOfPrefectures.ehime,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => print('クリックされました'),
+          ),
+        ),
+      ],
+    );
+  }
+}
