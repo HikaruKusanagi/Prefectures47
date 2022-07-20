d82046535d3b5a61b8672012e388fca0b0dfc2d4
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-20 21:51:22 +0900

    no message

---
+++ lib/kyushu/kyushu.widget/kyushuwidget5.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:prefectures47/name_of_prefectures.dart';
+
+class KyushuWidget5 extends StatelessWidget {
+  const KyushuWidget5({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 20),
+        Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              side: BorderSide(
+                color: Colors.black, //色
+                width: 2, //太さ
+              ),
+            ),
+            child: Text(
+              NameOfPrefectures.oita,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => print('クリックされました'),
+          ),
+        ),
+        SizedBox(width: 30),
+        Row(
+          children: [
+            Center(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  side: BorderSide(
+                    color: Colors.black, //色
+                    width: 2, //太さ
+                  ),
+                ),
+                child: Text(
+                  NameOfPrefectures.miyazaki,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () => print('クリックされました'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
