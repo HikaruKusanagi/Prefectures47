83b853a8a60ed1535c3e3731dbd532225280a95d
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-21 23:39:01 +0900

    no message

--- lib/tohoku/tohoku.widget/tohokuwidget.dart
+++ lib/tohoku/tohoku.widget/tohokuwidget.dart
@@ -2,9 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:prefectures47/name_of_prefectures.dart';
 
 class TohokuWidget extends StatelessWidget {
-  const TohokuWidget({
+  TohokuWidget({
     Key? key,
   }) : super(key: key);
+
+  List<String> imgURLs = [
+    //ここに表示したい画像URLを追加
+    'images/1_hokkaidou.png',
+    // 'images/2_touhoku1__aomori.png',
+    // 'images/2_touhoku2__iwate.png',
+    // 'images/2_touhoku3__miyagi.png',
+    // 'images/2_touhoku4__akita.png',
+    // 'images/2_touhoku5__yamagata.png',
+    // 'images/3_kantou1__ibaraki.png',
+    // 'images/3_kantou2__tochigi.png',
+    // 'images/3_kantou3__gunma.png',
+    // 'images/3_kantou4__saitama.png',
+    // 'images/3_kantou5__chiba.png',
+    // 'images/3_kantou6__tokyo.png',
+    // 'images/3_kantou7__kanagawa.png',
+    // 'images/4_chuubu1_yamanashi.png',
+    // 'images/4_chuubu2_nagano.png',
+    // 'images/4_chuubu3_niigata.png',
+    // 'images/4_chuubu4_toyama.png',
+    // 'images/4_chuubu5_ishikawa.png',
+    // 'images/4_chuubu6_fukui.png',
+    // 'images/4_chuubu7_shizuoka.png',
+    // 'images/4_chuubu8_aichi.png',
+    // 'images/4_chuubu9_gifu.png',
+    // 'images/5_kinki1_mie.png',
+    // 'images/5_kinki2_shiga.png',
+    // 'images/5_kinki3_kyouto.png',
+    // 'images/5_kinki4_osaka.png',
+    // 'images/5_kinki5_hyougo.png',
+    // 'images/5_kinki6_nara.png',
+    // 'images/6_chuugoku1_tottori.png',
+    // 'images/6_chuugoku2_shimane.png',
+    // 'images/6_chuugoku3_okayama.png',
+    // 'images/6_chuugoku4_hiroshima.png',
+    // 'images/6_chuugoku5_yamaguchi.png',
+    // 'images/7_shikoku1_kagawa.png',
+    // 'images/7_shikoku2_ehime.png',
+    // 'images/7_shikoku3_tokushima.png',
+    // 'images/7_shikoku4_kouchi.png',
+  ];
+
+  String result = '';
+
+  // String randomNumberToHand(int randomNumber) {
+  //   switch (randomNumber) {
+  //     case 0:
+  //       return 'images/1_hokkaidou.png';
+  //     case 1:
+  //       return 'images/2_touhoku1__aomori.png';
+  //     case 2:
+  //       return '✋';
+  //     default:
+  //       return '✊';
+  //   }
+  // }
+
+  void judge() {
+    // 北海道ボタンを選択し画面の北海道と合っていたら
+    if (NameOfPrefectures.hokkaido == imgURLs) {
+      result = '正解';
+    } else if (NameOfPrefectures.hokkaido != imgURLs) {
+      result = '不正解';
+    }
+  }
+
+  void selectedTodofu(String selectedTodofu) {
+    NameOfPrefectures.hokkaido = selectedTodofu;
+    judge();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +97,7 @@ class TohokuWidget extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            onPressed: () => print('クリックされました'),
+            onPressed: () => selectedTodofu(NameOfPrefectures.hokkaido),
           ),
         ),
         SizedBox(width: 30),
