1bec82daa029d366e50a8ce4a9c6a39bd757793d
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-22 23:48:42 +0900

    no message

--- lib/tohoku/tohokupage.dart
+++ lib/tohoku/tohokupage.dart
@@ -104,6 +104,7 @@ class _TohokuPageState extends State<TohokuPage> {
             child: ListView(children: [
               Row(
                 children: [
+                  SizedBox(width: 20),
                   Center(
                     child: TextButton(
                       style: TextButton.styleFrom(
