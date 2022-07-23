9865d5cdddc060a6bf1d796cf279c5ceda44aa77
HikaruKusanagi <hikarugp24@gmail.com>
2022-07-23 22:11:06 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prefectures47/home.dart';
 
 import 'package:prefectures47/mapselection.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MapSelection(),
+      home: const MyHomePage(),
     );
   }
 }
